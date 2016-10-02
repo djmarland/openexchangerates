7958e9828b60840c154bb7e1b9e18c1a6fe17245
David Marland <davidm@rland.me.uk>
2016-10-02 21:39:25 +0100

    Fix composer

--- src/OpenExchangeRates/Client.php
+++ src/OpenExchangeRates/Client.php
@@ -1,7 +1,23 @@
 <?php
 namespace Djmarland\OpenExchangeRates;
 
+use GuzzleHttp\ClientInterface;
+
 class Client
 {
+    /**
+     * @var ClientInterface
+     */
+    private $httpClient;
 
+    /**
+     * @var string
+     */
+    private $apiKey;
+
+    public function __construct(ClientInterface $httpClient, string $apiKey)
+    {
+        $this->httpClient = $httpClient;
+        $this->apiKey = $apiKey;
+    }
 }
---
+++ src/OpenExchangeRates/Result.php
@@ -0,0 +1,7 @@
+<?php
+namespace Djmarland\OpenExchangeRates;
+
+class Result
+{
+
+}
